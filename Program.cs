9b076c9b49dff4f2fab6e492393728e6df49f327
Nedal <nedalosama3@gmail.com>
2024-07-29 22:07:12 +0300

    EvenNumber

--- Program.cs
+++ Program.cs
@@ -4,34 +4,63 @@ namespace Collections_Assignment
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             #region ReverseList
-            ArrayList list = new ArrayList() { 1, 2, 3, 4, 5 };
-            Reverse(list);
+            /*  ArrayList list = new ArrayList() { 1, 2, 3, 4, 5 };
+              Reverse(list);
 
-            foreach (var item in list)
+              foreach (var item in list)
+              {
+                  Console.WriteLine(item);
+              }
+          }
+
+          private static void Reverse(ArrayList list)
+          {
+              int left = 0;
+              int right = list.Count - 1;
+
+              while (left < right)
+              {
+                  var temp = list[left];
+                  list[left] = list[right];
+                  list[right] = temp;
+
+                  left++;
+                  right--;
+              }
+          }*/
+            #endregion
+
+
+            #region EvenNumber
+        /*    List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> evenNumbers = GetEvenNumbers(numbers);
+
+            foreach (var num in evenNumbers)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(num);
             }
+
         }
 
-        private static void Reverse(ArrayList list)
+        private static List<int> GetEvenNumbers(List<int> numbers)
         {
-            int left = 0;
-            int right = list.Count - 1;
+            List<int> evenNumbers = new List<int>();
 
-            while (left < right)
+            foreach (int number in numbers)
             {
-                var temp = list[left];
-                list[left] = list[right];
-                list[right] = temp;
-
-                left++;
-                right--;
+                if (number % 2 == 0)
+                {
+                    evenNumbers.Add(number);
+                }
             }
-        }
-        #endregion
 
+            return evenNumbers;
+        */
+        #endregion
+    } 
     }
 }
