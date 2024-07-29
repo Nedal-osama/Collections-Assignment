9a911034d9f21cfc329028d19447053cf887040e
Nedal <nedalosama3@gmail.com>
2024-07-29 22:00:09 +0300

    ReverseList

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Collections_Assignment
+﻿using System.Collections;
+
+namespace Collections_Assignment
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region ReverseList
+            ArrayList list = new ArrayList() { 1, 2, 3, 4, 5 };
+            Reverse(list);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
+
+        private static void Reverse(ArrayList list)
+        {
+            int left = 0;
+            int right = list.Count - 1;
+
+            while (left < right)
+            {
+                var temp = list[left];
+                list[left] = list[right];
+                list[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+        #endregion
+
     }
 }
